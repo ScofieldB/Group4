31b0601b97dc17ac9429bc009b32e4bb39d8de70
Brendan Scofield <B.Scofield@connect.qut.edu.au>
2014-05-13 22:26:21 +1000

    minor bug fix
    Minor bug fix of input validation for adding and deleting users via
    System admin users

--- Hospital/Hospital/Adminfrm.cs
+++ Hospital/Hospital/Adminfrm.cs
@@ -39,17 +39,22 @@ namespace Hospital {
             Admin admin = new Admin();
             string Surname = "";
 
-            Surname = admin.addUser(Usernametxt.Text, Rolecmb.SelectedItem.ToString());
+            try {
+                Surname = admin.addUser(Usernametxt.Text, Rolecmb.SelectedItem.ToString());
 
 
-            if (Surname == "") {
-                MessageBox.Show("Username " + Usernametxt.Text + " is not a valid userame"
-                                + Surname, "User Not Added",
-                MessageBoxButtons.OK, MessageBoxIcon.Error);
-            } else {
-                MessageBox.Show("Username " + Usernametxt.Text + " has now been added to system with password"
-                                + Surname, "User Added",
-                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                if (Surname == "") {
+                    MessageBox.Show("Username " + Usernametxt.Text + " is not a valid userame"
+                                    + Surname, "User Not Added",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                } else {
+                    MessageBox.Show("Username " + Usernametxt.Text + " has now been added to system with password: "
+                                    + Surname, "User Added",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            } catch (Exception) {
+                MessageBox.Show("Please input a valid username and select a role", "User Not Added",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
 
@@ -58,12 +63,17 @@ namespace Hospital {
         private void Deletebtn_Click(object sender, EventArgs e) {
             Admin admin = new Admin();
 
-            admin.deleteUser(Usernametxt.Text);
+            try {
+                admin.deleteUser(Usernametxt.Text);
 
 
-            MessageBox.Show("UserID " + Usernametxt.Text + " has now been deleted from system", "User deleted",
-                MessageBoxButtons.OK, MessageBoxIcon.Error);
-            Usernametxt.Text = "";
+                MessageBox.Show("UserID " + Usernametxt.Text + " has now been deleted from system", "User deleted",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Usernametxt.Text = "";
+            } catch {
+                MessageBox.Show("Please input a valid username to be deleted", "User Not Deleted",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         /*
