5888a159a2575677ada527ab19033c85ef5943fd
ChristopherWeir <ya-mum1@live.com.au>
2014-05-24 14:09:53 +1000

    button update

--- Hospital/Hospital/AdminRepfrm.Designer.cs
+++ Hospital/Hospital/AdminRepfrm.Designer.cs
@@ -23,6 +23,7 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminRepfrm));
             this.label1 = new System.Windows.Forms.Label();
             this.TestRepbtn = new System.Windows.Forms.Button();
             this.LogBtn = new System.Windows.Forms.Button();
@@ -50,12 +51,17 @@
             // 
             // LogBtn
             // 
-            this.LogBtn.Location = new System.Drawing.Point(233, 13);
+            this.LogBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("LogBtn.BackgroundImage")));
+            this.LogBtn.FlatAppearance.BorderSize = 0;
+            this.LogBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LogBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
+            this.LogBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.LogBtn.Location = new System.Drawing.Point(209, 12);
             this.LogBtn.Name = "LogBtn";
-            this.LogBtn.Size = new System.Drawing.Size(75, 23);
+            this.LogBtn.Size = new System.Drawing.Size(99, 23);
             this.LogBtn.TabIndex = 2;
             this.LogBtn.Text = "Logout";
-            this.LogBtn.UseVisualStyleBackColor = true;
+            this.LogBtn.UseVisualStyleBackColor = false;
             this.LogBtn.Click += new System.EventHandler(this.LogBtn_Click);
             // 
             // BackBtn
