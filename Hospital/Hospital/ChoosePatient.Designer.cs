04174a87fb6109b94bc0ddc55e2edbcde1483d38
Brendan Scofield <B.Scofield@connect.qut.edu.au>
2014-05-29 00:10:24 +1000

    Now can press enter on choose patient screen

--- Hospital/Hospital/ChoosePatient.Designer.cs
+++ Hospital/Hospital/ChoosePatient.Designer.cs
@@ -33,10 +33,9 @@
             // 
             this.SearchSurnamelbl.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.SearchSurnamelbl.AutoSize = true;
-            this.SearchSurnamelbl.Location = new System.Drawing.Point(28, 44);
-            this.SearchSurnamelbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.SearchSurnamelbl.Location = new System.Drawing.Point(37, 54);
             this.SearchSurnamelbl.Name = "SearchSurnamelbl";
-            this.SearchSurnamelbl.Size = new System.Drawing.Size(131, 13);
+            this.SearchSurnamelbl.Size = new System.Drawing.Size(175, 17);
             this.SearchSurnamelbl.TabIndex = 0;
             this.SearchSurnamelbl.Text = "Surname being searched: ";
             // 
@@ -44,10 +43,9 @@
             // 
             this.Chooselbl.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Chooselbl.AutoSize = true;
-            this.Chooselbl.Location = new System.Drawing.Point(30, 102);
-            this.Chooselbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.Chooselbl.Location = new System.Drawing.Point(40, 126);
             this.Chooselbl.Name = "Chooselbl";
-            this.Chooselbl.Size = new System.Drawing.Size(198, 13);
+            this.Chooselbl.Size = new System.Drawing.Size(263, 17);
             this.Chooselbl.TabIndex = 1;
             this.Chooselbl.Text = "Please choose from one of the following:";
             // 
@@ -55,10 +53,10 @@
             // 
             this.chooseCmb.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.chooseCmb.FormattingEnabled = true;
-            this.chooseCmb.Location = new System.Drawing.Point(32, 118);
-            this.chooseCmb.Margin = new System.Windows.Forms.Padding(2);
+            this.chooseCmb.Location = new System.Drawing.Point(43, 145);
+            this.chooseCmb.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.chooseCmb.Name = "chooseCmb";
-            this.chooseCmb.Size = new System.Drawing.Size(386, 21);
+            this.chooseCmb.Size = new System.Drawing.Size(513, 24);
             this.chooseCmb.TabIndex = 2;
             this.chooseCmb.SelectedIndexChanged += new System.EventHandler(this.chooseCmb_SelectedIndexChanged);
             // 
@@ -71,10 +69,10 @@
             this.Confirmbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Confirmbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
             this.Confirmbtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.Confirmbtn.Location = new System.Drawing.Point(174, 167);
-            this.Confirmbtn.Margin = new System.Windows.Forms.Padding(2);
+            this.Confirmbtn.Location = new System.Drawing.Point(232, 206);
+            this.Confirmbtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Confirmbtn.Name = "Confirmbtn";
-            this.Confirmbtn.Size = new System.Drawing.Size(99, 23);
+            this.Confirmbtn.Size = new System.Drawing.Size(132, 28);
             this.Confirmbtn.TabIndex = 3;
             this.Confirmbtn.Text = "Confirm";
             this.Confirmbtn.UseVisualStyleBackColor = true;
@@ -82,19 +80,20 @@
             // 
             // ChoosePatient
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AcceptButton = this.Confirmbtn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(255)))), ((int)(((byte)(251)))));
             this.BackgroundImage = global::Hospital.Properties.Resources.pagebackground;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(452, 236);
+            this.ClientSize = new System.Drawing.Size(603, 290);
             this.Controls.Add(this.Confirmbtn);
             this.Controls.Add(this.chooseCmb);
             this.Controls.Add(this.Chooselbl);
             this.Controls.Add(this.SearchSurnamelbl);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ChoosePatient";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ChoosePatient";
