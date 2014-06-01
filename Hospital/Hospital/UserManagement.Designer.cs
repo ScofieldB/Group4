fac3febff17fdd614cd1df572df79ca77b413545
Brendan Scofield <B.Scofield@connect.qut.edu.au>
2014-06-01 10:04:47 +1000

    Admin can query what role a user is now

--- Hospital/Hospital/UserManagement.Designer.cs
+++ Hospital/Hospital/UserManagement.Designer.cs
@@ -31,6 +31,7 @@
             this.Newbtn = new System.Windows.Forms.Button();
             this.LogBtn = new System.Windows.Forms.Button();
             this.Updatebtn = new System.Windows.Forms.Button();
+            this.Querybtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Rolecmb
@@ -43,7 +44,7 @@
             "MedTech",
             "Receptionist",
             "Admin"});
-            this.Rolecmb.Location = new System.Drawing.Point(297, 138);
+            this.Rolecmb.Location = new System.Drawing.Point(304, 138);
             this.Rolecmb.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Rolecmb.Name = "Rolecmb";
             this.Rolecmb.Size = new System.Drawing.Size(229, 24);
@@ -53,7 +54,7 @@
             // 
             this.Rolelbl.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Rolelbl.AutoSize = true;
-            this.Rolelbl.Location = new System.Drawing.Point(249, 142);
+            this.Rolelbl.Location = new System.Drawing.Point(258, 144);
             this.Rolelbl.Name = "Rolelbl";
             this.Rolelbl.Size = new System.Drawing.Size(41, 17);
             this.Rolelbl.TabIndex = 19;
@@ -62,7 +63,7 @@
             // Usernametxt
             // 
             this.Usernametxt.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.Usernametxt.Location = new System.Drawing.Point(295, 110);
+            this.Usernametxt.Location = new System.Drawing.Point(304, 112);
             this.Usernametxt.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Usernametxt.Name = "Usernametxt";
             this.Usernametxt.Size = new System.Drawing.Size(229, 22);
@@ -72,7 +73,7 @@
             // 
             this.Usernamelbl.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Usernamelbl.AutoSize = true;
-            this.Usernamelbl.Location = new System.Drawing.Point(231, 113);
+            this.Usernamelbl.Location = new System.Drawing.Point(240, 115);
             this.Usernamelbl.Name = "Usernamelbl";
             this.Usernamelbl.Size = new System.Drawing.Size(58, 17);
             this.Usernamelbl.TabIndex = 17;
@@ -87,7 +88,7 @@
             this.Deletebtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Deletebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Deletebtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.Deletebtn.Location = new System.Drawing.Point(473, 204);
+            this.Deletebtn.Location = new System.Drawing.Point(38, 191);
             this.Deletebtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Deletebtn.Name = "Deletebtn";
             this.Deletebtn.Size = new System.Drawing.Size(132, 28);
@@ -105,7 +106,7 @@
             this.Newbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Newbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Newbtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.Newbtn.Location = new System.Drawing.Point(157, 204);
+            this.Newbtn.Location = new System.Drawing.Point(38, 127);
             this.Newbtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Newbtn.Name = "Newbtn";
             this.Newbtn.Size = new System.Drawing.Size(132, 28);
@@ -124,7 +125,7 @@
             this.LogBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
             this.LogBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.LogBtn.Location = new System.Drawing.Point(536, 22);
-            this.LogBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.LogBtn.Margin = new System.Windows.Forms.Padding(4);
             this.LogBtn.Name = "LogBtn";
             this.LogBtn.Size = new System.Drawing.Size(132, 28);
             this.LogBtn.TabIndex = 21;
@@ -134,12 +135,13 @@
             // 
             // Updatebtn
             // 
+            this.Updatebtn.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.Updatebtn.BackgroundImage = global::Hospital.Properties.Resources.buttonbackground;
             this.Updatebtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.Updatebtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.Updatebtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
             this.Updatebtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.Updatebtn.Location = new System.Drawing.Point(312, 204);
+            this.Updatebtn.Location = new System.Drawing.Point(38, 159);
             this.Updatebtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Updatebtn.Name = "Updatebtn";
             this.Updatebtn.Size = new System.Drawing.Size(132, 28);
@@ -147,6 +149,22 @@
             this.Updatebtn.Text = "Update User";
             this.Updatebtn.UseVisualStyleBackColor = true;
             this.Updatebtn.Click += new System.EventHandler(this.Updatebtn_Click);
+            // 
+            // Querybtn
+            // 
+            this.Querybtn.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.Querybtn.BackgroundImage = global::Hospital.Properties.Resources.buttonbackground;
+            this.Querybtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Querybtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Querybtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold);
+            this.Querybtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Querybtn.Location = new System.Drawing.Point(38, 94);
+            this.Querybtn.Name = "Querybtn";
+            this.Querybtn.Size = new System.Drawing.Size(132, 28);
+            this.Querybtn.TabIndex = 23;
+            this.Querybtn.Text = "Query User";
+            this.Querybtn.UseVisualStyleBackColor = true;
+            this.Querybtn.Click += new System.EventHandler(this.Querybtn_Click);
             // 
             // UserManagement
             // 
@@ -156,6 +174,7 @@
             this.BackgroundImage = global::Hospital.Properties.Resources.pagebackground;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(701, 342);
+            this.Controls.Add(this.Querybtn);
             this.Controls.Add(this.Updatebtn);
             this.Controls.Add(this.LogBtn);
             this.Controls.Add(this.Rolecmb);
@@ -185,5 +204,6 @@
         private System.Windows.Forms.Button Newbtn;
         private System.Windows.Forms.Button LogBtn;
         private System.Windows.Forms.Button Updatebtn;
+        private System.Windows.Forms.Button Querybtn;
     }
 }
